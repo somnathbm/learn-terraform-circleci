b0d559b09f5a1eb1d0790fecaa8443e483b707f2
somnath <somnathbm0@gmail.com>
2023-10-27 23:09:06 +0530

    disabled public block

--- main.tf
+++ main.tf
@@ -30,14 +30,14 @@ resource "aws_s3_bucket_ownership_controls" "app" {
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "app" {
-  bucket = aws_s3_bucket.app.id
+# resource "aws_s3_bucket_public_access_block" "app" {
+#   bucket = aws_s3_bucket.app.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
 
 resource "aws_s3_bucket_acl" "app" {
   depends_on = [
